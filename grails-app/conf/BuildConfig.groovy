ed8b88bf872b6c013ce3316c66d4168dbc4e343a
Jonathan <Jonathan@Jonathan-PC>
2014-11-30 19:23:47 -0500

    merge

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -61,6 +61,7 @@ grails.project.dependency.resolution = {
         compile ":scaffolding:2.1.2"
         compile ':cache:1.1.7'
         compile ":asset-pipeline:1.8.11"
+		compile ':geoip:0.3.3'
         // plugins needed at runtime but not for compilation
         runtime ":hibernate4:4.3.5.4" // or ":hibernate:3.6.10.16"
         runtime ":database-migration:1.4.0"
