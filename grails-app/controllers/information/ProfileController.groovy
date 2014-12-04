3aed85ba8a2442a779aa16151aa2abc3e04b144c
Matt Fleishman <matthew.fleishman@stonybrook.edu>
2014-12-04 10:55:31 -0500

    Delete Profile/Dates Update

--- grails-app/controllers/information/ProfileController.groovy
+++ grails-app/controllers/information/ProfileController.groovy
@@ -1,26 +1,27 @@
 package information
 
 import static org.springframework.http.HttpStatus.*
+import dating_system.UserDate;
 import grails.transaction.Transactional
 
 @Transactional(readOnly = true)
 class ProfileController {
 
-    static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
+	static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
 
-    def show(Profile profileInstance) {
+	def show(Profile profileInstance) {
 		boolean owns=session.activeProfileId.equals(profileInstance.profileId);
-        render view:'show',model:[profile:profileInstance,profileOwner:owns];
-    }
+		render view:'show',model:[profile:profileInstance,profileOwner:owns];
+	}
 
-    def create() {
-        respond new Profile(params)
-    }
-	
+	def create() {
+		respond new Profile(params)
+	}
+
 	def searchForm(){
 		render view: 'search'
 	}
-	
+
 	def search(){
 		def c = Profile.createCriteria()
 		def resultList = c.list{
@@ -56,23 +57,23 @@ class ProfileController {
 		render view: "searchList", model: [resultList: resultList]
 	}
 
-    @Transactional
-    def save() {
+	@Transactional
+	def save() {
 		println "Save"
 		Profile p = new Profile()
-		
+
 		p.owner = session.user;
-		
+
 		// Dates
 		p.creationDate = new Date();
 		p.lastModDate= new Date();
-		
+
 		// Strings
 		p.profileId = params.profileId;
 		p.mf = params.mf;
 		p.hobbies = params.hobbies;
 		p.hairColor = params.hairColor;
-		
+
 		// integer
 		p.datinGeoRange= Integer.parseInt(params.datinGeoRange);
 		p.datingAgeRangeStart= Integer.parseInt(params.datingAgeRangeStart);
@@ -81,79 +82,101 @@ class ProfileController {
 		p.age=Integer.parseInt(params.age);
 		// double
 		p.height = Double.parseDouble(params.height);
-		
+
 		p.save flush:true
 		println p.errors
-        request.withFormat {
-            form multipartForm {
-                flash.message = message(code: 'default.created.message', args: [message(code: 'profile.label', default: 'Profile'), p.id])
-                redirect action:"edit", model:[profileInstance:p]
-            }
-            '*' { respond p, [status: CREATED] }
-        }
-    }
+		request.withFormat {
+			form multipartForm {
+				flash.message = message(code: 'default.created.message', args: [
+					message(code: 'profile.label', default: 'Profile'),
+					p.id
+				])
+				redirect action:"edit", model:[profileInstance:p]
+			}
+			'*' { respond p, [status: CREATED] }
+		}
+	}
 
-    def edit() {
+	def edit() {
 		def profileInstance= Profile.findWhere(profileId:session.activeProfileId);
 		render view:'edit',model:[profileInstance:profileInstance];
-    }
+	}
 
-    @Transactional
-    def update() {
-		
+	@Transactional
+	def update() {
+
 		def p = Profile.findWhere(profileId:session.activeProfileId);
-		
+
 		// Dates
 		p.lastModDate= new Date();
-		
+
 		// Strings
 		p.mf = params.mf;
 		p.hobbies = params.hobbies;
 		p.hairColor = params.hairColor;
-		
+
 		// integer
 		p.datinGeoRange= Integer.parseInt(params.datinGeoRange);
 		p.datingAgeRangeStart= Integer.parseInt(params.datingAgeRangeStart);
 		p.datingAgeRangeEnd= Integer.parseInt(params.datingAgeRangeEnd);
 		p.weight= Integer.parseInt(params.weight);
-		
+
 		// double
 		p.height = Double.parseDouble(params.height);
-		
+
 		p.save flush:true
 
-        request.withFormat {
-            form multipartForm {
-                flash.message = message(code: 'default.updated.message', args: [message(code: 'Profile.label', default: 'Profile'), p.profileId])
-                redirect action:"edit", model:[profileInstance:p]
-            }
-            '*'{ respond p, [status: OK] }
-        }
-    }
+		request.withFormat {
+			form multipartForm {
+				flash.message = message(code: 'default.updated.message', args: [
+					message(code: 'Profile.label', default: 'Profile'),
+					p.profileId
+				])
+				redirect action:"edit", model:[profileInstance:p]
+			}
+			'*'{ respond p, [status: OK] }
+		}
+	}
 
-    @Transactional
-    def delete() {
+	@Transactional
+	def delete() {
 
 		def p = Profile.findWhere(profileId:session.activeProfileId);
+		if(UserDate.findWhere(profile1:p)!=null||UserDate.findWhere(profile2:p)!=null){
+			request.withFormat {
+				form multipartForm {
+					flash.message ="Cannot delete profile."
+					redirect action:"edit", controller:"profile",id:p.profileId, method:"GET"
+				}
+				'*'{ render status: NO_CONTENT }
+			}
+		}
+		else{
+			p.delete flush:true
 
-        p.delete flush:true
+			request.withFormat {
+				form multipartForm {
+					flash.message = message(code: 'default.deleted.message', args: [
+						message(code: 'Profile.label', default: 'Profile'),
+						p.id
+					])
+					redirect action:"index", controller:"app", method:"GET"
+				}
+				'*'{ render status: NO_CONTENT }
+			}
+		}
+	}
 
-        request.withFormat {
-            form multipartForm {
-                flash.message = message(code: 'default.deleted.message', args: [message(code: 'Profile.label', default: 'Profile'), p.id])
-                redirect action:"index", controller:"app", method:"GET"
-            }
-            '*'{ render status: NO_CONTENT }
-        }
-    }
-
-    protected void notFound() {
-        request.withFormat {
-            form multipartForm {
-                flash.message = message(code: 'default.not.found.message', args: [message(code: 'profile.label', default: 'Profile'), params.id])
-                redirect action: "index", method: "GET"
-            }
-            '*'{ render status: NOT_FOUND }
-        }
-    }
+	protected void notFound() {
+		request.withFormat {
+			form multipartForm {
+				flash.message = message(code: 'default.not.found.message', args: [
+					message(code: 'profile.label', default: 'Profile'),
+					params.id
+				])
+				redirect action: "index", method: "GET"
+			}
+			'*'{ render status: NOT_FOUND }
+		}
+	}
 }
